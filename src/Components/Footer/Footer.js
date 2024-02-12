c8b00309d55bafcc14df6e5da84cf5eb22fbd2e7
Roman 111 <romanvydysh7@gmail.com>
2024-02-12 18:08:03 +0200

    footer-fix

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Footer.scss"
 import { FooterDesktop } from './FooterDesktop/FooterDesktop'
+import { FooterTabs } from './FooterTabs/FooterTabs'
 import { FooterMobile } from './FooterMobile/FooterMobile'
 export const Footer = () => {
   return (
@@ -9,6 +10,9 @@ export const Footer = () => {
         <img/>
         <div className="footer_desktop">
             <FooterDesktop />
+        </div>
+        <div className="footer_tabs">
+            <FooterTabs/>
         </div>
         <div className="footer_mobile">
             <FooterMobile/>
