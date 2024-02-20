9396af6a401147d6368bf10864540cd3b021e544
aspen801 <boyko.bogdan45@gmail.com>
2024-02-20 21:32:50 +0200

    catalog page structure fix

--- src/pages/CatalogPage/CatalogPage.js
+++ src/pages/CatalogPage/CatalogPage.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import "./CatalogPage.scss"
-import { NavigationCatalog } from '../../Components/NavigationCatalog/NavigationCatalog'
+import React from "react";
+import "./CatalogPage.scss";
+import { NavigationCatalog } from "../../Components/NavigationCatalog/NavigationCatalog";
+import CatalogGrid from "../../Components/CatalogGrid/CatalogGrid";
 const CatalogPage = () => {
   return (
-    <>
-        <NavigationCatalog/>
-    </>
-  )
-}
+    <div className="catalog-page">
+      <div className="catalog-content">
+        <NavigationCatalog />
+        <div className="catalog-grid-section">
+          <CatalogGrid />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CatalogPage
+export default CatalogPage;
