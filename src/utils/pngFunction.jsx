82a8f66b3580f74753aa36e2268aed1b8d827503
Bogdan Boyko <boyko.bogdan45@gmail.com>
2024-04-18 00:21:47 +0300

    fix: hero section fix

--- src/utils/pngFunction.jsx
+++ src/utils/pngFunction.jsx
@@ -1,41 +1,39 @@
-import SotsLeft from "../resources/png/SotsLeft.png"
-import SotsRight from "../resources/png/SotsRight.png"
-import SotsTablet from "../resources/png/SotsTablet.png"
-import SotsPhone from "../resources/png/SotsPhone.png"
-import SotMouse from '../resources/png/SotMouse.png'
-import SotHeadphones from '../resources/png/SotHeadphones.png'
-import SotDisplay from '../resources/png/SotDisplay.png'
-import SotKeyBoard from '../resources/png/SotKeyBoard.png'
-import SotArmchair from '../resources/png/SotArmchair.png'
-import SotLaptop from '../resources/png/SotLaptop.png'
+import SotArmchair from "../resources/png/SotArmchair.png";
+import SotDisplay from "../resources/png/SotDisplay.png";
+import SotHeadphones from "../resources/png/SotHeadphones.png";
+import SotKeyBoard from "../resources/png/SotKeyBoard.png";
+import SotLaptop from "../resources/png/SotLaptop.png";
+import SotMouse from "../resources/png/SotMouse.png";
+import SotsLeft from "../resources/png/SotsLeft.png";
+import SotsPhone from "../resources/png/SotsPhone.png";
+import SotsRight from "../resources/png/SotsRight.png";
+import SotsTablet from "../resources/png/SotsTablet.png";
 
 const givePng = (name) => {
-    switch(name){
-        case "SotsLeft":
-            return <img src={SotsLeft} alt={name} className={name} />
-        case "SotsRight":
-            return <img src={SotsRight} alt={name} className={name} />
-        case "SotsTablet":
-            return <img src={SotsTablet} alt={name} className={name} />
-        case "SotsPhone":
-            return <img src={SotsPhone} alt={name} className={name} />
-        case "SotMouse":
-            return <img src={SotMouse} alt={name} className={name} />
-        case "SotHeadphones":
-            return <img src={SotHeadphones} alt={name} className={name} />
-        case "SotDisplay":
-            return <img src={SotDisplay} alt={name} className={name} />
-        case "SotKeyBoard":
-            return <img src={SotKeyBoard} alt={name} className={name} />
-        case "SotArmchair":
-            return <img src={SotArmchair} alt={name} className={name} />
-        case "SotLaptop":
-            return <img src={SotLaptop} alt={name} className={name} />
-        default:
-            return null
-    }
+  switch (name) {
+    case "SotsLeft":
+      return <img src={SotsLeft} alt={name} className={name} />;
+    case "SotsRight":
+      return <img src={SotsRight} alt={name} className={name} />;
+    case "SotsTablet":
+      return <img src={SotsTablet} alt={name} className={name} />;
+    case "SotsPhone":
+      return <img src={SotsPhone} alt={name} className={name} />;
+    case "SotMouse":
+      return <img src={SotMouse} alt={name} className={name} />;
+    case "SotHeadphones":
+      return <img src={SotHeadphones} alt={name} className={name} />;
+    case "SotDisplay":
+      return <img src={SotDisplay} alt={name} className={name} />;
+    case "SotKeyBoard":
+      return <img src={SotKeyBoard} alt={name} className={name} />;
+    case "SotArmchair":
+      return <img src={SotArmchair} alt={name} className={name} />;
+    case "SotLaptop":
+      return <img src={SotLaptop} alt={name} className={name} />;
+    default:
+      return null;
+  }
+};
 
-}
-
-
-export default givePng
+export default givePng;
